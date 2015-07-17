48bb78093a735c7b91a586265b976ff86303f54a
Dimitri 'phaazon' Sabadie <dimitri.sabadie@gmail.com>
2015-07-17 19:17:27 +0200

    Added Regions to Buffer.

--- src/Graphics/Luminance/Buffer.hs
+++ src/Graphics/Luminance/Buffer.hs
@@ -10,31 +10,79 @@
 
 module Graphics.Luminance.Buffer where
 
-import Control.Monad.IO.Class ( MonadIO )
-import Data.Word ( Word32 )
+import Control.Monad.IO.Class ( MonadIO(..) )
+import Control.Monad.State ( State, get, put, runState )
+import Data.Bits ( (.|.) )
+import Data.Word ( Word8, Word32 )
+import Foreign.Ptr ( castPtr, plusPtr )
+import Foreign.Storable ( sizeOf )
 import Graphics.GL
 import Graphics.Luminance.Memory
 
-newtype Buffer a = Buffer { bufferID :: GC GLuint }
+newtype Buffer rw = Buffer { bufferID :: GC GLuint }
 
--- WRONG! We cannot call glDeleteBuffers that way…
-mkBuffers :: (MonadIO m)
-          => GLenum
-          -> [Word32]
-          -> m [Buffer a]
-mkBuffers target sizes = liftIO $ do
-    p <- mallocArray nb
-    glGenBuffers nb p
-    buffers <- peekArray p nb
-    traverse (createStorageNBuffer p) $ zip buffers sizes
+type ReadBuffer = Buffer ReadOnly
+type WriteBuffer = Buffer WriteOnly
+
+data ReadOnly = ReadOnly deriving (Eq,Ord,Show)
+
+data WriteOnly = WriteOnly deriving (Eq,Ord,Show)
+
+{-
+readBuffer :: (MonadIO m)
+           => GLenum
+           -> Word32
+-}
+mkBuffer :: (MonadIO m)
+         => rw
+         -> GLenum
+         -> GLbitfield
+         -> Word32
+         -> m (Buffer rw)
+mkBuffer _ target flags size = liftIO $ do
+  p <- malloc
+  glGenBuffers 1 p
+  peek p >>= \bid -> do
+    createStorage bid target flags size
+    Buffer <$> embedGC bid (glDeleteBuffers 1 p)
+
+mkBufferWithRegions :: (MonadIO m)
+                    => rw
+                    -> GLenum
+                    -> GLbitfield
+                    -> BuildRegion a
+                    -> m (Buffer rw,a)
+mkBufferWithRegions rw target flags buildRegions =
+    (,)
+      <$> mkBuffer rw target flags bytes
+      <*> pure a
   where
-    nb = length sizes
-    createStorageNBuffer p (bid,size) = do
-      createStorage bid target size
-      Buffer <$> embedGC bid (glDeleteBuffers nb p)
+    (a,bytes) = runState (runBuildRegion buildRegions) 0
 
-createStorage :: GLuint -> GLenum -> Word32 -> IO ()
-createStorage bid target size = do
+createStorage :: GLuint -> GLenum -> GLbitfield -> Word32 -> IO ()
+createStorage bid target flags size = do
   glBindBuffer target bid
-  glBufferStorage target size nullPtr flags
+  glBufferStorage target (fromIntegral size) nullPtr flags
   glBindBuffer target 0
+
+data Region a = Region {
+    regionPtr :: Ptr a
+  , regionSize :: RegionSize a
+  } deriving (Eq,Show)
+
+newtype RegionSize a = RegionSize Word32 deriving (Eq,Show)
+
+sizeOfR :: forall a. (Storable a) => RegionSize a -> Word32
+sizeOfR (RegionSize size) = fromIntegral (sizeOf (undefined :: a)) * size
+
+newtype BuildRegion a = BuildRegion { runBuildRegion :: State Word32 a }
+  deriving (Applicative,Functor,Monad)
+
+newRegion :: forall a. (Storable a) => Word32 -> BuildRegion (Region a)
+newRegion size = BuildRegion $ do
+    offset <- get
+    put $ offset + sizeOfR sizeR
+    pure $ Region (nullPtr `plusPtr` fromIntegral offset) sizeR
+  where
+    sizeR :: RegionSize a
+    sizeR = RegionSize size
--- src/Graphics/Luminance/Framebuffer.hs
+++ src/Graphics/Luminance/Framebuffer.hs
@@ -10,9 +10,11 @@
 
 module Graphics.Luminance.Framebuffer where
 
-import Graphics.Luminance.Memory ( GC )
+import Control.Monad.IO.Class ( MonadIO(..) )
+import Graphics.GL
+import Graphics.Luminance.Memory
 
-data Framebuffer c d = Framebuffer { framebufferID :: GC GLint }
+newtype Framebuffer c d = Framebuffer { framebufferID :: GC GLint }
 
 type ColorFramebuffer t f = Framebuffer (Color t f) Complete
 type DepthFramebuffer t f = Framebuffer Complete (Depth t f)
@@ -26,13 +28,14 @@ data Depth t f
 data Complete
 
 defaultFramebuffer :: (MonadIO m) => m CompleteFramebuffer
-defaultFramebuffer = Framebuffer <$> embedGC -1 (pure ())
+defaultFramebuffer = Framebuffer <$> embedGC (-1) (pure ())
 
 mkFramebuffer :: (MonadIO m) => m (Framebuffer c d)
 mkFramebuffer = liftIO $ do
   p <- malloc
   glGenFramebuffers 1 p
-  peek p >>= $ \fb -> Framebuffer <$> embdGC fb (glDeleteFramebuffers 1 p)
+  peek p >>= \fid ->
+    Framebuffer <$> embedGC (fromIntegral fid) (glDeleteFramebuffers 1 p)
 
 colorFramebuffer :: (MonadIO m) => t -> f -> m (ColorFramebuffer t f)
 colorFramebuffer _ _ = mkFramebuffer
@@ -40,12 +43,19 @@ colorFramebuffer _ _ = mkFramebuffer
 depthFramebuffer :: (MonadIO m) => t -> f -> m (DepthFramebuffer t f)
 depthFramebuffer _ _ = mkFramebuffer
 
-colorDepthFramebuffer :: (MonadIO m) => t -> f -> m (ColorDepthFramebuffer t f)
-colorDepthFramebuffer _ _ = mkFramebuffer
+colorDepthFramebuffer :: (MonadIO m)
+                      => ct
+                      -> cf
+                      -> dt
+                      -> df
+                      -> m (ColorDepthFramebuffer ct cf dt df)
+colorDepthFramebuffer _ _ _ _ = mkFramebuffer
 
+{-
 attachColorTexture :: (MonadIO m)
                    => Framebuffer (Color t f) d
                    -> Texture t f
                    -> m (Either String (Framebuffer Complete d))
 attachColorTexture fb tex = liftIO $ do
   -- ?!
+-}
