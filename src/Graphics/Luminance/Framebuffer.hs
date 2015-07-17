382770a7eb0a2ed2721e53d636537250de6abbf1
Dimitri Sabadie <dimitri.sabadie@gmail.com>
2015-07-17 16:08:43 +0200

    Update Framebuffer.hs

--- src/Graphics/Luminance/Framebuffer.hs
+++ src/Graphics/Luminance/Framebuffer.hs
@@ -12,10 +12,12 @@ module Graphics.Luminance.Framebuffer where
 
 import Graphics.Luminance.Memory ( GC )
 
-data Framebuffer :: * -> * -> * where
-  ColorOnlyFramebuffer  :: GC GLuint -> Framebuffer (Color t f) NoDepth
-  DepthOnlyFramebuffer  :: GC GLuint -> Framebuffer NoColor (Depth t f)
-  ColorDepthFramebuffer :: GC GLuint -> Framebuffer (Color ct cf) (Depth dt df)
+data Framebuffer c d = Framebuffer { framebufferID :: GC GLint }
+
+type ColorFramebuffer t f = Framebuffer (Color t f) NoDepth
+type DepthFramebuffer t f = Framebuffer NoColor (Depth t f)
+type ColorDepthFramebuffer ct cf dt df = Framebuffer (Color ct cf) (Depth dt df)
+type CompleteFramebuffer = Framebuffer NoColor NoDepth
 
 data Color t f
 
@@ -24,3 +26,12 @@ data Depth t f
 data NoColor
 
 data NoDepth
+
+defaultFramebuffer :: (MonadIO m) => m CompleteFramebuffer
+defaultFramebuffer = Framebuffer <$> embedGC -1 (pure ())
+
+mkFramebuffer :: (MonadIO m) => m (Framebuffer c d)
+mkFramebuffer nb = liftIO $ do
+  p <- malloc
+  glGenFramebuffers 1 p
+  peek p >>= $ \fb -> Framebuffer <$> embdGC fb (glDeleteFramebuffers 1 p)
