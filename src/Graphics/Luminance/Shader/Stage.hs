0273d05280fd980a2c9cce07147ab86bc86b7af1
Dimitri Sabadie <dimitri.sabadie@gmail.com>
2015-07-22 18:21:08 +0200

    Replaced the Memory module by MonadResource.

--- src/Graphics/Luminance/Shader/Stage.hs
+++ src/Graphics/Luminance/Shader/Stage.hs
@@ -12,8 +12,8 @@ module Graphics.Luminance.Shader.Stage where
 
 import Control.Applicative ( liftA2 )
 import Control.Monad.IO.Class ( MonadIO(..) )
+import Control.Monad.Trans.Resource ( MonadResource, register )
 import Graphics.GL
-import Graphics.Luminance.Memory
 import Foreign.C.String ( peekCString, withCString )
 import Foreign.Marshal.Alloc ( alloca )
 import Foreign.Marshal.Array ( allocaArray )
@@ -21,29 +21,29 @@ import Foreign.Marshal.Utils ( with )
 import Foreign.Ptr ( castPtr, nullPtr )
 import Foreign.Storable ( peek )
 
-newtype Stage = Stage { stageID :: GC GLuint }
+newtype Stage = Stage { stageID :: GLuint }
 
-tcsShader :: (MonadIO m) => String -> m (Either String Stage)
+tcsShader :: (MonadIO m,MonadResource m) => String -> m (Either String Stage)
 tcsShader = mkShader GL_TESS_CONTROL_SHADER
 
-tesShader :: (MonadIO m) => String -> m (Either String Stage)
+tesShader :: (MonadIO m,MonadResource m) => String -> m (Either String Stage)
 tesShader = mkShader GL_TESS_EVALUATION_SHADER
 
-vertexShader :: (MonadIO m) => String -> m (Either String Stage)
+vertexShader :: (MonadIO m,MonadResource m) => String -> m (Either String Stage)
 vertexShader = mkShader GL_VERTEX_SHADER
 
-geometryShader :: (MonadIO m) => String -> m (Either String Stage)
+geometryShader :: (MonadIO m,MonadResource m) => String -> m (Either String Stage)
 geometryShader = mkShader GL_GEOMETRY_SHADER
 
-fragmentShader :: (MonadIO m) => String -> m (Either String Stage)
+fragmentShader :: (MonadIO m,MonadResource m) => String -> m (Either String Stage)
 fragmentShader = mkShader GL_FRAGMENT_SHADER
 
-computeShader :: (MonadIO m) => String -> m (Either String Stage)
+computeShader :: (MonadIO m,MonadResource m) => String -> m (Either String Stage)
 computeShader = mkShader GL_COMPUTE_SHADER
 
-mkShader :: (MonadIO m) => GLenum -> String -> m (Either String Stage)
+mkShader :: (MonadIO m,MonadResource m) => GLenum -> String -> m (Either String Stage)
 mkShader target src = do
-  liftIO $ do
+  (sid,compiled,cl) <- liftIO $ do
     sid <- glCreateShader target
     withCString src $ \cstr -> do
       with cstr $ \pcstr -> glShaderSource sid 1 pcstr nullPtr
@@ -51,9 +51,12 @@ mkShader target src = do
       compiled <- isCompiled sid
       ll <- clogLength sid
       cl <- clog ll sid
-      if
-        | compiled  -> (Right . Stage) <$> embedGC sid (glDeleteShader sid)
-        | otherwise -> pure $ Left cl
+      pure (sid,compiled,cl)
+  if
+    | compiled  -> do
+        _ <- register $ glDeleteShader sid
+        pure . Right $ Stage sid
+    | otherwise -> pure $ Left cl
 
 isCompiled :: GLuint -> IO Bool
 isCompiled sid = do
