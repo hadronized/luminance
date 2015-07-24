6c9b8ba0ab3845c43308696bb9e891d1be9f92e0
Dimitri Sabadie <dimitri.sabadie@gmail.com>
2015-07-24 17:09:15 +0200

    Regions should be corrected now.

--- src/Graphics/Luminance/Buffer.hs
+++ src/Graphics/Luminance/Buffer.hs
@@ -11,70 +11,67 @@
 module Graphics.Luminance.Buffer where
 
 import Control.Monad.IO.Class ( MonadIO(..) )
+import Control.Monad.RWS ( RWS, ask, get, evalRWS, execRWS, put )
 import Control.Monad.Trans.Resource ( MonadResource, register )
-import Control.Monad.State ( State, get, put, runState )
 import Data.Bits ( (.|.) )
 import Data.Word ( Word32 )
 import Foreign.Marshal.Alloc ( malloc )
 import Foreign.Marshal.Array ( peekArray, pokeArray )
-import Foreign.Ptr ( Ptr, nullPtr, plusPtr )
+import Foreign.Ptr ( Ptr, castPtr, nullPtr, plusPtr )
 import Foreign.Storable ( Storable(..) )
 import Graphics.GL
 
-newtype Buffer rw = Buffer { bufferID :: GLuint }
-
-type ReadBuffer = Buffer ReadOnly
-type WriteBuffer = Buffer WriteOnly
+newtype Buffer = Buffer { bufferID :: GLuint }
 
 data ReadOnly = ReadOnly deriving (Eq,Ord,Show)
-
 data WriteOnly = WriteOnly deriving (Eq,Ord,Show)
+
+mkBuffer :: (MonadIO m,MonadResource m)
+         => GLbitfield
+         -> Word32
+         -> m (Buffer,Ptr ())
+mkBuffer flags size = do
+  (bid,p,mapped) <- liftIO $ do
+    p <- malloc
+    glGenBuffers 1 p
+    bid <- peek p
+    mapped <- createStorage bid flags size
+    pure (bid,p,mapped)
+  _ <- register $ glDeleteBuffers 1 p
+  pure (Buffer bid,mapped)
+
+createStorage :: GLuint -> GLbitfield -> Word32 -> IO (Ptr ())
+createStorage bid flags size = do
+    glBindBuffer universalTarget bid
+    glBufferStorage universalTarget bytes nullPtr flags
+    ptr <- glMapBufferRange universalTarget 0 bytes flags
+    glBindBuffer universalTarget 0
+    pure ptr
+  where
+    bytes = fromIntegral size
+
+mkBufferWithRegions :: (MonadIO m,MonadResource m)
+                    => GLbitfield
+                    -> BuildRegion rw a
+                    -> m a
+mkBufferWithRegions flags buildRegions = do
+    (_,mapped) <- mkBuffer flags bytes
+    pure . fst $ evalRWS built mapped 0
+  where
+    built = runBuildRegion buildRegions
+    (bytes,_) = execRWS built nullPtr 0
 
 readBuffer :: (MonadIO m,MonadResource m)
            => BuildRegion ReadOnly a
-           -> m (ReadBuffer,a)
+           -> m a
 readBuffer = mkBufferWithRegions $
   GL_MAP_READ_BIT .|. GL_MAP_PERSISTENT_BIT .|. GL_MAP_COHERENT_BIT
 
 writeBuffer :: (MonadIO m,MonadResource m)
             => BuildRegion WriteOnly a
-            -> m (WriteBuffer,a)
+            -> m a
 writeBuffer = mkBufferWithRegions $
   GL_MAP_WRITE_BIT .|. GL_MAP_PERSISTENT_BIT .|. GL_MAP_COHERENT_BIT
-
-mkBuffer :: (MonadIO m,MonadResource m)
-         => GLbitfield
-         -> Word32
-         -> m (Buffer rw)
-mkBuffer flags size = do
-  (bid,p) <- liftIO $ do
-    p <- malloc
-    glGenBuffers 1 p
-    bid <- peek p
-    createStorage bid flags size
-    pure (bid,p)
-  _ <- register $ glDeleteBuffers 1 p
-  pure $ Buffer bid
-
-mkBufferWithRegions :: (MonadIO m,MonadResource m)
-                    => GLbitfield
-                    -> BuildRegion rw a
-                    -> m (Buffer rw,a)
-mkBufferWithRegions flags buildRegions =
-    (,)
-      <$> mkBuffer flags bytes
-      <*> pure a
-  where
-    (a,bytes) = runState (runBuildRegion buildRegions) 0
-
-createStorage :: GLuint -> GLbitfield -> Word32 -> IO ()
-createStorage bid flags size = do
-  glBindBuffer universalTarget bid
-  glBufferStorage universalTarget (fromIntegral size) nullPtr flags
-  glBindBuffer universalTarget 0
-
-universalTarget :: GLenum
-universalTarget = GL_COPY_WRITE_BUFFER
 
 data Region rw a = Region {
     regionPtr :: Ptr a
@@ -86,14 +83,16 @@ newtype RegionSize a = RegionSize Word32 deriving (Eq,Num,Show)
 sizeOfR :: forall a. (Storable a) => RegionSize a -> Word32
 sizeOfR (RegionSize size) = fromIntegral (sizeOf (undefined :: a)) * size
 
-newtype BuildRegion rw a = BuildRegion { runBuildRegion :: State Word32 a }
-  deriving (Applicative,Functor,Monad)
+newtype BuildRegion rw a = BuildRegion {
+    runBuildRegion :: RWS (Ptr ()) () Word32 a
+  } deriving (Applicative,Functor,Monad)
 
 newRegion :: forall rw a. (Storable a) => Word32 -> BuildRegion rw (Region rw a)
 newRegion size = BuildRegion $ do
     offset <- get
     put $ offset + sizeOfR regionS
-    pure $ Region (nullPtr `plusPtr` fromIntegral offset) regionS
+    ptr <- ask
+    pure $ Region (castPtr $ ptr `plusPtr` fromIntegral offset) regionS
   where
     regionS :: RegionSize a
     regionS = RegionSize size
@@ -125,3 +124,6 @@ writeAt (Region p (RegionSize nb)) i a
 
 writeAt' :: (MonadIO m,Storable a) => Region WriteOnly a -> Word32 -> a -> m ()
 writeAt' (Region p _) i a = liftIO $ pokeElemOff p (fromIntegral i) a
+
+universalTarget :: GLenum
+universalTarget = GL_COPY_WRITE_BUFFER
--- src/Graphics/Luminance/Texture.hs
+++ src/Graphics/Luminance/Texture.hs
@@ -35,3 +35,23 @@ data CompareFunc
     deriving (Eq,Show)
 
 newtype Unit = Unit Natural deriving (Enum,Eq,Integral,Num,Ord,Real,Show)
+
+-- |Texture.
+--
+--            +-- Underlying channel type. Could be 'CInts', 'CUInts'
+--            |   or 'CFloats'.
+--            |
+--            | +-- Channel format. Should be composed of 'CR', 'CG' and/or
+--            | |   'CB' and channel sizes, like 'C8', 'C16' and/or 'C32'.
+--            | |
+--            | | +-- Dimension of the 'Texture'. Either 'D1', 'D2', 'D3' or
+--            | | |   'Cube'.
+--            | | |
+--            | | | +-- Is the texture 'Layered' or 'Unlayered'?
+--            | | | |
+--            v v v v
+-- @'Texture' t c d l@.
+newtype Texture t c d l
+
+
+Texture Floats ((CR,C8),(CG,C8),(CB,C8)) D2 Unlayered
