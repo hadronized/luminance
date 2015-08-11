fe732ff4c7cc9864fbb3e1323ef20599bd2cfde5
Dimitri Sabadie <dimitri.sabadie@gmail.com>
2015-08-11 11:51:39 +0200

    DirectGeometry and IndexedGeometry added.
      · the createGeometry function now creates a new kind of Geometry if indices are passed or not ;
      · the createGeometry function now takes a mode parameter to change the primitive mode.

--- src/Graphics/Luminance/Geometry.hs
+++ src/Graphics/Luminance/Geometry.hs
@@ -27,13 +27,35 @@ import Graphics.Luminance.Tuple
 vertexBindingIndex :: GLuint
 vertexBindingIndex = 0
 
-newtype Geometry = Geometry { vertexArrayID :: GLuint } deriving (Eq,Show)
+data VertexArray = VertexArray {
+    vertexArrayID :: GLuint
+  , vertexArrayMode :: GLenum
+  , vertexArrayCount :: GLsizei
+  } deriving (Eq,Show)
+  
+newtype Geometry
+  | DirectGeometry VertexArray
+  | IndexedGeometry VertexArray
+    deriving (Eq,Show)
 
+data GeometryMode
+  = Point
+  | Line
+  | Triangle
+    deriving (Eq,Show)
+
+fromGeometryMode :: GeometryMode -> GLenum
+fromGeometryMode m = case m of
+  Point    -> GL_POINTS
+  Line     -> GL_LINES
+  Triangle -> GL_TRIANGLES
+  
 createGeometry :: forall f m t v. (Foldable f,MonadIO m,MonadResource m,Storable v,Traversable t,Vertex v)
-               => t v
-               -> f Word32
-               -> m Geometry
-createGeometry vertices indices = do
+                      => t v
+                      -> Maybe (f Word32)
+                      -> GeometryMode
+                      -> m Geometry
+createGeometry vertices indices mode = do
     -- create the vertex array object (OpenGL-side)
     vid <- liftIO . alloca $ \p -> do
       glCreateVertexArrays 1 p
@@ -42,15 +64,16 @@ createGeometry vertices indices = do
     -- vertex buffer
     (vreg :: Region W v,vbo) <- createBuffer_ $ newRegion (fromIntegral vertNb)
     writeWhole vreg vertices
-    -- element buffer
-    (ireg :: Region W Word32,ibo) <- createBuffer_ $ newRegion (fromIntegral indNb)
-    writeWhole ireg indices
-    -- connect the buffers to the VAO
-    liftIO $ do
-      glVertexArrayVertexBuffer vid vertexBindingIndex (bufferID vbo) 0 (fromIntegral $ sizeOf (undefined :: v))
-      glVertexArrayElementBuffer vid (bufferID ibo)
-      setFormatV vid 0 (undefined :: v)
-    pure $ Geometry vid
+    liftIO $ glVertexArrayVertexBuffer vid vertexBindingIndex (bufferID vbo) 0 (fromIntegral $ sizeOf (undefined :: v))
+    setFormatV vid 0 (undefined :: v)
+    -- element buffer, if required
+    case indices of
+      Just indices' -> 
+        (ireg :: Region W Word32,ibo) <- createBuffer_ $ newRegion (fromIntegral indNb)
+        writeWhole ireg indices'
+        glVertexArrayElementBuffer vid (bufferID ibo)
+        pure . IndexedGeometry $ VertexArray vid mode' indNb
+      Nothing -> pure . DirectGeometry $ VertexArray vid mode' vertNb
   where
     vertNb = length vertices
     indNb  = length indices
