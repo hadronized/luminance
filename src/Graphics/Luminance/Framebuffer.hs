213aa119a2aa0148087e8050a1e8ef97b50efc3f
Dimitri Sabadie <dimitri.sabadie@gmail.com>
2015-08-12 18:09:36 +0200

    Added attachmentLength into FramebufferAttachment.
    That function is used to determine the length of an attachment type.
    
    For color attachments, it’ll recursively scan down the tuple of formats
    (:.) and gets its length.
    
    For depth attachments, because depth attachment should be singleton, the
    size is either 0 or 1.
    
    We can use that function to re-implement hasColor and hasDepth by
    comparing to 0 (>0).

--- src/Graphics/Luminance/Framebuffer.hs
+++ src/Graphics/Luminance/Framebuffer.hs
@@ -53,9 +53,11 @@ createFramebuffer w h mipmaps = do
   fid <- liftIO . alloca $ \p -> do
     glCreateFramebuffers 1 p
     peek p
-  --configureFramebufferBuffers fid (toBool hasColor) (toBool hasDepth)
   createFramebufferTexture (ColorAttachment 0) (Proxy :: Proxy c) fid w h mipmaps
   createFramebufferTexture DepthAttachment (Proxy :: Proxy d) fid w h mipmaps
+  let colorOutputsLength = attachmentLength (Proxy :: Proxy c)
+      hasDepth = attachmentLength (Proxy :: Proxy d) == 1
+  configureFramebufferBuffers fid colorOutputsLength hasDepth
   _ <- register . with fid $ glDeleteFramebuffers 1
   pure $ Framebuffer fid w h mipmaps
 
@@ -68,15 +70,18 @@ class FramebufferAttachment a where
                            -> Natural
                            -> Natural
                            -> m ()
+  attachmentLength         :: Proxy a -> Natural
 
 instance FramebufferAttachment () where
   createFramebufferTexture _ _ _ _ _ _ = pure ()
+  attachmentLength _ = 0
 
 instance (Pixel (Format t c)) => FramebufferAttachment (Format t c) where
   createFramebufferTexture ca _ fid w h mipmaps = do
     tex :: Texture2D (Format t c) <- createTexture w h mipmaps
     liftIO $ glNamedFramebufferTexture fid (fromAttachment ca)
       (textureID tex) 0
+  attachmentLength _ = 1
 
 instance (FramebufferAttachment a,FramebufferAttachment b) => FramebufferAttachment (a :. b) where
   createFramebufferTexture ca _ fid w h mipmaps = case ca of
@@ -84,8 +89,15 @@ instance (FramebufferAttachment a,FramebufferAttachment b) => FramebufferAttachm
       _ <- createFramebufferTexture ca (Proxy :: Proxy a) fid w h mipmaps
       createFramebufferTexture (ColorAttachment $ succ i) (Proxy :: Proxy b) fid w h mipmaps
     _ -> pure ()
+  attachmentLength _ = attachmentLength (Proxy :: Proxy a) + attachmentLength (Proxy :: Proxy b)
 
-{-
-configureFramebufferBuffers :: (MonadIO m) => GLint -> Bool -> Bool -> m ()
-configureFramebufferBuffers fid hasColor hasDepth
--}
+configureFramebufferBuffers :: (MonadIO m,MonadResource m)
+                            => GLuint
+                            -> Natural
+                            -> Bool
+                            -> m ()
+configureFramebufferBuffers fid colorOutputs hasDepth
+  | colorOutputs == 0 && hasDepth = do
+      glNamedFramebufferDrawBuffer fid GL_NONE
+      glNamedFramebufferReadBuffer fid GL_NONE
+  | otherwise = pure ()
