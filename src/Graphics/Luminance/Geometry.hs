51bf4d9977a21f39aa032b0885f0903402e058ca
Dimitri Sabadie <dimitri.sabadie@gmail.com>
2015-08-15 23:41:22 +0200

    Storable instances for V Nat *.

--- src/Graphics/Luminance/Geometry.hs
+++ src/Graphics/Luminance/Geometry.hs
@@ -15,7 +15,9 @@ import Control.Monad.Trans.Resource ( MonadResource, register )
 import Data.Proxy ( Proxy(..) )
 import Data.Word ( Word32 )
 import Foreign.Marshal.Alloc ( alloca )
+import Foreign.Marshal.Array ( peekArray, pokeArray )
 import Foreign.Marshal.Utils ( with )
+import Foreign.Ptr ( Ptr, castPtr )
 import Foreign.Storable ( Storable(..) )
 import GHC.TypeLits ( KnownNat, Nat, natVal )
 import Graphics.GL
@@ -85,6 +87,36 @@ data V :: Nat -> * -> * where
   V2 :: !a -> !a -> V 2 a
   V3 :: !a -> !a -> !a -> V 3 a
   V4 :: !a -> !a -> !a -> !a -> V 4 a
+
+instance (Storable a) => Storable (V 1 a) where
+  sizeOf _ = sizeOf (undefined :: a)
+  alignment _ = alignment (undefined :: a)
+  peek p = fmap V1 $ peek (castPtr p :: Ptr a)
+  poke p (V1 x) = poke (castPtr p) x
+
+instance (Storable a) => Storable (V 2 a) where
+  sizeOf _ = 2 * sizeOf (undefined :: a)
+  alignment _ = alignment (undefined :: a)
+  peek p = do
+    [x,y] <- peekArray 2 (castPtr p :: Ptr a)
+    pure $ V2 x y
+  poke p (V2 x y) = pokeArray (castPtr p) [x,y]
+
+instance (Storable a) => Storable (V 3 a) where
+  sizeOf _ = 3 * sizeOf (undefined :: a)
+  alignment _ = alignment (undefined :: a)
+  peek p = do
+    [x,y,z] <- peekArray 3 (castPtr p :: Ptr a)
+    pure $ V3 x y z
+  poke p (V3 x y z) = pokeArray (castPtr p) [x,y,z]
+
+instance (Storable a) => Storable (V 4 a) where
+  sizeOf _ = 4 * sizeOf (undefined :: a)
+  alignment _ = alignment (undefined :: a)
+  peek p = do
+    [x,y,z,w] <- peekArray 4 (castPtr p :: Ptr a)
+    pure $ V4 x y z w
+  poke p (V4 x y z w) = pokeArray (castPtr p) [x,y,z,w]
 
 class Vertex v where
   setFormatV :: (MonadIO m) => GLuint -> GLuint -> Proxy v -> m ()
