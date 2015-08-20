6c6242628515edaca7843767f2d4d0317b031f8c
Dimitri Sabadie <dimitri.sabadie@gmail.com>
2015-08-20 17:57:38 +0200

    Added depth test enabling/disabling in drawGeometry.

---
+++ src/Graphics/Luminance/RenderCmd.hs
@@ -0,0 +1,16 @@
+-----------------------------------------------------------------------------
+-- |
+-- Copyright   : (C) 2015 Dimitri Sabadie
+-- License     : BSD3
+--
+-- Maintainer  : Dimitri Sabadie <dimitri.sabadie@gmail.com>
+-- Stability   : experimental
+-- Portability : portable
+-----------------------------------------------------------------------------
+
+module Graphics.Luminance.RenderCmd where
+
+import Graphics.Luminance.Blending
+import Graphics.Luminance.Shader.Uniform ( Uniformed )
+
+data RenderCmd rw c d a = RenderCmd BlendingMode BlendingFactor Bool (Uniformed a)
