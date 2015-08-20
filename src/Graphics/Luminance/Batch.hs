13217549770b9c1fae9ff0cc77be1bea813c3d61
Dimitri Sabadie <dimitri.sabadie@gmail.com>
2015-08-20 11:37:00 +0200

    Integrated the Uniformed type in the treatSPBatch function.

--- src/Graphics/Luminance/Batch.hs
+++ src/Graphics/Luminance/Batch.hs
@@ -10,6 +10,7 @@
 
 module Graphics.Luminance.Batch where
 
+import Control.Monad ( void )
 import Control.Monad.IO.Class ( MonadIO(..) )
 import Data.Foldable ( traverse_ )
 import Foreign.Ptr ( nullPtr )
@@ -17,7 +18,7 @@ import Graphics.GL
 import Graphics.Luminance.Framebuffer ( Framebuffer(..) )
 import Graphics.Luminance.Geometry ( Geometry(..), VertexArray(..) )
 import Graphics.Luminance.Shader.Program ( Program(..) )
-
+import Graphics.Luminance.Shader.Uniform ( Uniformed(..) )
 
 -- TEST ONLY
 import Data.Bits
@@ -29,6 +30,7 @@ data FBBatch rw c d = FBBatch {
 
 data SPBatch = SPBatch {
     spBatchShaderProgram :: Program
+  , spBatchUniformed     :: Uniformed ()
   , spBatchGeometries    :: [Geometry]
   }
 
@@ -42,8 +44,10 @@ drawGeometry g = case g of
     glDrawElements mode ixNb GL_UNSIGNED_INT nullPtr
 
 treatSPBatch :: (MonadIO m) => SPBatch -> m ()
-treatSPBatch (SPBatch prog geometries) = do
-  liftIO $ glUseProgram (programID prog)
+treatSPBatch (SPBatch prog uniformed geometries) = do
+  liftIO $ do
+    glUseProgram (programID prog)
+    void $ updateUniforms uniformed
   traverse_ drawGeometry geometries
 
 treatFBBatch :: (MonadIO m) => FBBatch rw c d -> m ()
