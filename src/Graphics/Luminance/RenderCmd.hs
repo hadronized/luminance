1cc84693f4e9f9b8c73f79b6d5887964ed3b4f54
Dimitri 'phaazon' Sabadie <dimitri.sabadie@gmail.com>
2015-08-21 10:23:21 +0200

    Functor & Applicative instances for RenderCmd.

--- src/Graphics/Luminance/RenderCmd.hs
+++ src/Graphics/Luminance/RenderCmd.hs
@@ -14,3 +14,10 @@ import Graphics.Luminance.Blending
 import Graphics.Luminance.Shader.Uniform ( Uniformed(..) )
 
 data RenderCmd rw c d a = RenderCmd (Maybe (BlendingMode,BlendingFactor)) Bool (Uniformed a)
+
+instance Functor (RenderCmd rw c d) where
+  fmap f (RenderCmd blending depthTest a) = RenderCmd blending depthTest (fmap f a)
+
+instance Applicative (RenderCmd rw c d) where
+  pure x = RenderCmd Nothing True (pure x)
+  RenderCmd _ _ f <*> RenderCmd blending depthTest a = RenderCmd blending depthTest (f <*> a)
