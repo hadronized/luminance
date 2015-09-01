2dea362794b3b8cd560352de5255655f78d684b8
Dimitri 'phaazon' Sabadie <dimitri.sabadie@gmail.com>
2015-09-02 01:56:56 +0200

    Query documentation.

--- src/Graphics/Luminance/Query.hs
+++ src/Graphics/Luminance/Query.hs
@@ -19,21 +19,27 @@ import Foreign.C.String ( peekCString )
 import Foreign.Ptr ( castPtr )
 import Graphics.GL
 
+-- Nicer version of 'glGetString'.
 getString :: (MonadIO m) => GLenum -> m String
 getString name = liftIO $ glGetString name >>= peekCString . castPtr
 
+-- |Get the OpenGL vendor 'String'.
 getGLVendor :: (MonadIO m) => m String
 getGLVendor = getString GL_VENDOR
 
+-- |Get the OpenGL renderer 'String'.
 getGLRenderer :: (MonadIO m) => m String
 getGLRenderer = getString GL_RENDERER
 
+-- |Get the OpenGL version 'String'.
 getGLVersion :: (MonadIO m) => m String
 getGLVersion = getString GL_VERSION
 
+-- |Get the GLSL version 'String'.
 getGLSLVersion :: (MonadIO m) => m String
 getGLSLVersion = getString GL_SHADING_LANGUAGE_VERSION
 
+-- |Retrieve all the supported OpenGL extensions as 'String's.
 getGLExtensions :: (MonadIO m) => m [String]
 getGLExtensions = liftIO $ do
   num <- alloca $ \num -> glGetIntegerv GL_NUM_EXTENSIONS num >> peek num
