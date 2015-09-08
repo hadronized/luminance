8a2c358c53f6dc8eae59302cde0f0d205e356a6f
Dimitri Sabadie <dimitri.sabadie@gmail.com>
2015-09-08 16:18:48 +0200

    Update Cmd.hs

--- src/Graphics/Luminance/Cmd.hs
+++ src/Graphics/Luminance/Cmd.hs
@@ -18,5 +18,18 @@ data Cmd a = Cmd { runCmd :: IO a } deriving (Applicative,Functor,Monad)
 draw :: FBBatch rw c d -> Cmd (Output c d)
 draw = Cmd . runFBBatch
 
-blit :: FBBatch r c d -> FBBatch w c d -> Cmd (Output c d)
-blit r w = Cmd (framebufferBlit r w)
+blit :: FBBatch r c d
+     -> FBBatch w c d
+     -> Int
+     -> Int
+     -> Natural
+     -> Natural
+     -> Int
+     -> Int
+     -> Natural
+     -> Natural
+     -> FramebufferBlitMask
+     -> Filter
+     -> Cmd (Output c d)
+blit r w rx ry rwidth rheight wx wy wwidth wheight mask flt =
+  Cmd (framebufferBlit r w rx ry rwidth rheight wx wy wwidth wheight mask flt)
