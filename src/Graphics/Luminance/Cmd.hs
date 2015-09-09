b6f060f53d3f9026d9ad3d6054ad63fda59395f9
Dimitri 'phaazon' Sabadie <dimitri.sabadie@gmail.com>
2015-09-09 23:46:39 +0200

    Cmd work.

--- src/Graphics/Luminance/Cmd.hs
+++ src/Graphics/Luminance/Cmd.hs
@@ -16,13 +16,18 @@ import Graphics.Luminance.RW ( Readable, Writable )
 import Graphics.Luminance.Texture ( Filter )
 import Numeric.Natural ( Natural )
 
+-- |Command type. Used to accumulate GPU commands. Use 'runCmd' to execute
+-- the whole chain of commands.
 newtype Cmd a = Cmd { runCmd :: IO a } deriving (Applicative,Functor,Monad)
 
+-- |Draw a framebuffer batch and return the framebuffer’s output.
 draw :: FBBatch rw c d -> Cmd (Output c d)
 draw fbb = Cmd $ do
   runFBBatch fbb
   pure . framebufferOutput $ fbBatchFramebuffer fbb
 
+-- |Blit a framebuffer batch onto another and return the framebuffer’s output of the write
+-- framebuffer.
 blit :: (Readable r,Writable w)
      => FBBatch r c d
      -> FBBatch w c d
