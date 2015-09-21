e58d5cd3cc68f8f9a91a1a5220a0fe32ff1c65ed
Dimitri 'phaazon' Sabadie <dimitri.sabadie@gmail.com>
2015-09-22 00:16:56 +0200

    Framebuffer documentation.

--- src/Graphics/Luminance/Core/Framebuffer.hs
+++ src/Graphics/Luminance/Core/Framebuffer.hs
@@ -34,19 +34,37 @@ import Numeric.Natural ( Natural )
 ---------------------------------------------------------------------------------
 -- Framebuffer ------------------------------------------------------------------
 
+-- |A 'Framebuffer' represents two buffers: a /color/ buffer and /depth/ buffer.
+-- You can select which one you want and specify the formats to use by providing 'Pixel'
+-- types. If you want to mute a buffer, use '()'.
 data Framebuffer rw c d = Framebuffer {
     framebufferID :: GLuint
   , framebufferOutput :: Output c d
   }
 
+-- |A 'Framebuffer' with the /depth/ buffer muted.
 type ColorFramebuffer rw c = Framebuffer rw c ()
+
+-- |A 'Framebuffer' with the /color/ buffer muted. Can be used to implement fast pre-passes.
 type DepthFramebuffer rw d = Framebuffer rw () d
 
-newtype FramebufferError = IncompleteFramebuffer String deriving (Eq,Show)
-
-class HasFramebufferError a where
-  fromFramebufferError :: FramebufferError -> a
-
+-- |@'createFramebuffer' w h mipmaps@ creates a new 'Framebuffer' with dimension @w * h@ and
+-- allocating spaces for @mipmaps@ level of textures. The textures are created by providing a
+-- correct type.
+--
+-- For the color part, you can pass either:
+--
+-- - '()': that will mute the color buffer of the framebuffer;
+-- - @'Format' t c@: that will create a single texture with the wished color format;
+-- - or @a ':.' b@: that will create a chain of textures; 'a' and 'b' cannot be '()'.
+--
+-- For the depth part, you can pass either:
+--
+-- - '()': that will mute the depth buffer of the framebuffer;
+-- - @'Format' t c@: that will create a single texture with the wished depth format.
+--
+-- Finally, the @rw@ parameter can be set to 'R', 'W' or 'RW' to specify which kind of framebuffer
+-- access you’ll need.
 createFramebuffer :: forall c d e m rw. (HasFramebufferError e,MonadError e m,MonadIO m,MonadResource m,FramebufferColorAttachment c,FramebufferColorRW rw,FramebufferDepthAttachment d,FramebufferTarget rw)
                   => Natural
                   -> Natural
@@ -66,6 +84,7 @@ createFramebuffer w h mipmaps = do
     | status == GL_FRAMEBUFFER_COMPLETE -> pure $ Framebuffer fid (Output colorTexs depthTex)
     | otherwise -> throwError . fromFramebufferError . IncompleteFramebuffer $ translateFramebufferStatus status
 
+-- Translate OpenGL framebuffer status into human readable versions.
 translateFramebufferStatus :: GLenum -> String
 translateFramebufferStatus status = case status of
   GL_FRAMEBUFFER_UNDEFINED                     -> "undefined default read or write framebuffer"
@@ -81,6 +100,7 @@ translateFramebufferStatus status = case status of
 --------------------------------------------------------------------------------
 -- Framebuffer attachment ------------------------------------------------------
 
+-- Framebuffer attachment.
 data Attachment
   = ColorAttachment Natural
   | DepthAttachment
@@ -94,6 +114,7 @@ fromAttachment a = case a of
 --------------------------------------------------------------------------------
 -- Framebuffer color attachment ------------------------------------------------
 
+-- |Typeclass of possible framebuffer color attachments.
 class FramebufferColorAttachment c where
   addColorOutput :: (MonadIO m,MonadResource m)
                  => GLuint
@@ -117,9 +138,12 @@ instance (ColorPixel a,ColorPixel b,FramebufferColorAttachment a,FramebufferColo
     (d1,tex1) <- addColorOutput fid (succ ca) w h mipmaps (Proxy :: Proxy b)
     pure $ (d0 + d1,tex0 :. tex1)
 
+-- Given the maximum number of attachments in a color attachment, retrieve the list of OpenGL
+-- enumerations to represent all the color buffers.
 colorAttachmentsFromMax :: Natural -> [GLenum]
 colorAttachmentsFromMax m = [fromAttachment (ColorAttachment a) | a <- [0..m-1]]
 
+-- Set the OpenGL color buffers.
 setColorBuffers :: forall m proxy rw. (FramebufferColorRW rw,MonadIO m)
                 => GLuint 
                 -> Natural
@@ -135,6 +159,7 @@ setColorBuffers fid colorOutputNb _ = case colorOutputNb of
 --------------------------------------------------------------------------------
 -- Framebuffer depth attachment ------------------------------------------------
 
+-- |Typeclass of possible framebuffer depth attachments.
 class FramebufferDepthAttachment d where
   addDepthOutput :: (MonadIO m,MonadResource m)
                  => GLuint
@@ -150,6 +175,7 @@ instance FramebufferDepthAttachment () where
 instance (Pixel (Format t (CDepth d))) => FramebufferDepthAttachment (Format t (CDepth d)) where
   addDepthOutput fid w h mipmaps proxy = fmap (True,) $ addOutput fid DepthAttachment w h mipmaps proxy
 
+-- Create a renderbuffer used to mute depth information and link it to a framebuffer.
 setDepthRenderbuffer :: (MonadIO m,MonadResource m)
                      => GLuint
                      -> Natural
@@ -163,6 +189,7 @@ setDepthRenderbuffer fid w h = do
 --------------------------------------------------------------------------------
 -- Framebuffer color read/write configuration ----------------------------------
 
+-- |Typeclass used to implement read/write operation per color attachment.
 class FramebufferColorRW rw where
   setFramebufferColorRW :: (MonadIO m) => GLuint -> Natural -> proxy rw -> m ()
 
@@ -179,6 +206,7 @@ instance FramebufferColorRW RW where
 --------------------------------------------------------------------------------
 -- Framebuffer read/write target configuration ---------------------------------
 
+-- |Framebuffer representation of 'R', 'W' and 'RW'.
 class FramebufferTarget rw where
   framebufferTarget :: proxy rw -> GLenum
 
@@ -194,6 +222,7 @@ instance FramebufferTarget RW where
 --------------------------------------------------------------------------------
 -- Framebuffer outputs ---------------------------------------------------------
 
+-- Create a new texture and link it to the framebuffer.
 addOutput :: forall m p proxy. (MonadIO m,MonadResource m,Pixel p)
           => GLuint
           -> Attachment
@@ -211,17 +240,19 @@ addOutput fid ca w h mipmaps _ = do
 --------------------------------------------------------------------------------
 -- Framebuffer textures accessors ----------------------------------------------
 
--- FIXME: name
+-- |Given a 'Format', inject it into a 'Texture2D' to handle it.
 type family TexturizeFormat a :: * where
   TexturizeFormat ()           = ()
   TexturizeFormat (Format t c) = Texture2D (Format t c)
   TexturizeFormat (a :. b)     = TexturizeFormat a :. TexturizeFormat b
 
--- TODO: use Output c d instead of (TexturizeFormat c,TexturizeFormat d)
+-- |Framebuffer output.
 data Output c d = Output (TexturizeFormat c) (TexturizeFormat d)
 
 --------------------------------------------------------------------------------
 -- Framebuffer blitting --------------------------------------------------------
+
+-- |Mask for framebuffer blit operation.
 data FramebufferBlitMask
   = BlitColor
   | BlitDepth
@@ -234,6 +265,7 @@ fromFramebufferBlitMask mask = case mask of
   BlitDepth -> GL_DEPTH_BUFFER_BIT
   BlitBoth  -> GL_COLOR_BUFFER_BIT .|. GL_DEPTH_BUFFER_BIT
 
+-- Blit two framebuffers.
 framebufferBlit :: (MonadIO m,Readable r,Writable w)
                 => Framebuffer r c d
                 -> Framebuffer w c' d'
@@ -264,5 +296,14 @@ framebufferBlit src dst srcX srcY srcW srcH dstX dstY dstW dstH mask flt = liftI
 --------------------------------------------------------------------------------
 -- Special framebuffers --------------------------------------------------------
 
+-- |The default 'Framebuffer' represents the screen (back buffer with double buffering).
 defaultFramebuffer :: Framebuffer RW () ()
 defaultFramebuffer = Framebuffer 0 (Output () ())
+
+---------------------------------------------------------------------------------
+-- Framebuffer errors -----------------------------------------------------------
+
+newtype FramebufferError = IncompleteFramebuffer String deriving (Eq,Show)
+
+class HasFramebufferError a where
+  fromFramebufferError :: FramebufferError -> a
