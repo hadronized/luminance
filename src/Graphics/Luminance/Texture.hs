2aabf58df3700ff5dfadc0e4e0c405e74e27afe2
Dimitri Sabadie <dimitri.sabadie@gmail.com>
2015-10-06 13:08:08 +0200

    Export list and documentation for textures.

--- src/Graphics/Luminance/Texture.hs
+++ src/Graphics/Luminance/Texture.hs
@@ -23,11 +23,21 @@ module Graphics.Luminance.Texture (
   , uploadSub
   , fillSub
     -- * Available textures
+    -- ** 1D textures
+  , Texture1D
+  , texture1DW
     -- ** 2D textures
   , Texture2D
   , texture2DW
   , texture2DH
+    -- ** 3D textures
+  , Texture3D
+  , texture3DW
+  , texture3DH
+  , texture3DD
   ) where
   
 import Graphics.Luminance.Core.Texture
+import Graphics.Luminance.Core.Texture1D
 import Graphics.Luminance.Core.Texture2D
+import Graphics.Luminance.Core.Texture3D
