d61c863621844c7190b9541f58d07f56a2965bcd
Dimitri Sabadie <dimitri.sabadie@gmail.com>
2015-10-15 23:51:39 +0200

    Dropped the V type so that we can use linear’s one.

--- src/Graphics/Luminance/Core/Vertex.hs
+++ src/Graphics/Luminance/Core/Vertex.hs
@@ -8,55 +8,22 @@
 -- Portability : portable
 ----------------------------------------------------------------------------
 
-module Graphics.Luminance.Core.Vertex where
+module Graphics.Luminance.Core.Vertex (
+    VertexAttribute(..)
+  , Vertex(..)
+  , vertexBindingIndex
+  , module Linear.V
+  ) where
 
 import Control.Monad.IO.Class ( MonadIO(..) )
 import Data.Int ( Int32 )
 import Data.Proxy ( Proxy(..) )
 import Data.Word ( Word32 )
-import Foreign.Marshal.Array ( peekArray, pokeArray )
-import Foreign.Ptr ( Ptr, castPtr )
-import Foreign.Storable ( Storable(..) )
-import GHC.TypeLits ( KnownNat, Nat, natVal )
+import Foreign.Storable ( Storable )
+import GHC.TypeLits ( KnownNat, natVal )
 import Graphics.GL
 import Graphics.Luminance.Core.Tuple
-
--- FIXME: use linear’s one?
-data V :: Nat -> * -> * where
-  V1 :: !a -> V 1 a
-  V2 :: !a -> !a -> V 2 a
-  V3 :: !a -> !a -> !a -> V 3 a
-  V4 :: !a -> !a -> !a -> !a -> V 4 a
-
-instance (Storable a) => Storable (V 1 a) where
-  sizeOf _ = sizeOf (undefined :: a)
-  alignment _ = alignment (undefined :: a)
-  peek p = fmap V1 $ peek (castPtr p :: Ptr a)
-  poke p (V1 x) = poke (castPtr p) x
-
-instance (Storable a) => Storable (V 2 a) where
-  sizeOf _ = 2 * sizeOf (undefined :: a)
-  alignment _ = alignment (undefined :: a)
-  peek p = do
-    [x,y] <- peekArray 2 (castPtr p :: Ptr a)
-    pure $ V2 x y
-  poke p (V2 x y) = pokeArray (castPtr p) [x,y]
-
-instance (Storable a) => Storable (V 3 a) where
-  sizeOf _ = 3 * sizeOf (undefined :: a)
-  alignment _ = alignment (undefined :: a)
-  peek p = do
-    [x,y,z] <- peekArray 3 (castPtr p :: Ptr a)
-    pure $ V3 x y z
-  poke p (V3 x y z) = pokeArray (castPtr p) [x,y,z]
-
-instance (Storable a) => Storable (V 4 a) where
-  sizeOf _ = 4 * sizeOf (undefined :: a)
-  alignment _ = alignment (undefined :: a)
-  peek p = do
-    [x,y,z,w] <- peekArray 4 (castPtr p :: Ptr a)
-    pure $ V4 x y z w
-  poke p (V4 x y z w) = pokeArray (castPtr p) [x,y,z,w]
+import Linear.V
 
 -- |A vertex might have several attributes. The types of those attributes have to implement the
 -- 'VertexAttribute' typeclass in order to be used as vertex attributes.
