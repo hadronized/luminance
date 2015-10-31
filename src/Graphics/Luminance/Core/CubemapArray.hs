7c054d6fe614e17ea75207764c49945baac2fe8b
Dimitri 'phaazon' Sabadie <dimitri.sabadie@gmail.com>
2015-10-31 14:48:35 +0100

    Cubemap arrays migrated to gl32.

--- src/Graphics/Luminance/Core/CubemapArray.hs
+++ src/Graphics/Luminance/Core/CubemapArray.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE CPP #-}
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE KindSignatures #-}
 {-# LANGUAGE ScopedTypeVariables #-}
@@ -15,9 +16,10 @@
 
 module Graphics.Luminance.Core.CubemapArray where
 
+import Data.Foldable ( for_ )
 import Data.Proxy ( Proxy(..) )
-import Data.Vector.Storable ( unsafeWith )
-import Foreign.Ptr ( castPtr )
+import Data.Vector.Storable as V ( concat, unsafeWith )
+import Foreign.Ptr ( castPtr, nullPtr )
 import GHC.TypeLits ( KnownNat, Nat, natVal )
 import Graphics.Luminance.Core.Cubemap ( CubeFace, fromCubeFace )
 import Graphics.Luminance.Core.Texture ( BaseTexture(..), Texture(..) )
@@ -41,17 +43,37 @@ instance (KnownNat n, Pixel f) => Texture (CubemapArray n f) where
   toBaseTexture = cubemapArrayBase
   textureTypeEnum _ = GL_TEXTURE_CUBE_MAP_ARRAY
   textureSize (CubemapArray _ w h) = (w,h)
+#if GL45_BACKEND
   textureStorage _ tid levels (w,h) =
     glTextureStorage3D tid levels (pixelIFormat (Proxy :: Proxy f)) (fromIntegral w)
       (fromIntegral h) (fromIntegral $ natVal (Proxy :: Proxy n))
+#elif GL32_BACKEND
+  textureStorage _ tid levels (w,h) = do
+      glBindTexture GL_TEXTURE_CUBE_MAP_ARRAY tid
+      for_ [0..levels-1] $ \lvl -> glTexImage3D GL_TEXTURE_CUBE_MAP_ARRAY lvl
+        (fromIntegral $ pixelIFormat pf)(fromIntegral w) (fromIntegral h)
+        (fromIntegral $ natVal (Proxy :: Proxy n)) 0 (fromIntegral $ pixelFormat pf)
+        (fromIntegral $ pixelType pf) nullPtr
+    where
+      pf = Proxy :: Proxy f
+#endif
+#if GL45_BACKEND
   transferTexelsSub _ tid (layer,x,y,f) (w,h) texels =
       unsafeWith texels $ glTextureSubImage3D tid 0 (fromIntegral x) (fromIntegral y)
         (fromCubeFace f + fromIntegral layer*6) (fromIntegral w) (fromIntegral h) 1 fmt
         typ . castPtr
+#elif GL32_BACKEND
+  transferTexelsSub _ tid (layer,x,y,f) (w,h) texels = do
+      glBindTexture GL_TEXTURE_CUBE_MAP_ARRAY tid
+      unsafeWith texels $ glTexSubImage3D GL_TEXTURE_CUBE_MAP_ARRAY 0 (fromIntegral x)
+        (fromIntegral y) (fromCubeFace f + fromIntegral layer*6) (fromIntegral w) (fromIntegral h)
+        1 fmt typ . castPtr
+#endif
     where
       proxy = Proxy :: Proxy f
       fmt = pixelFormat proxy
       typ = pixelType proxy
+#if GL45_BACKEND
   fillTextureSub _ tid (layer,x,y,f) (w,h) filling =
       unsafeWith filling $ glClearTexSubImage tid 0 (fromIntegral x) (fromIntegral y) 
         (fromCubeFace f + fromIntegral layer*6) (fromIntegral w) (fromIntegral h) 1
@@ -60,3 +82,7 @@ instance (KnownNat n, Pixel f) => Texture (CubemapArray n f) where
       proxy = Proxy :: Proxy f
       fmt = pixelFormat proxy
       typ = pixelType proxy
+#elif GL32_BACKEND
+  fillTextureSub proxy tid o (w,h) filling =
+    transferTexelsSub proxy tid o (w,h) (V.concat $ replicate (fromIntegral $ w*h) filling)
+#endif
