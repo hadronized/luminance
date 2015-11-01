21c8e21d936643345b4ca412cf384d7787c09791
Dimitri 'phaazon' Sabadie <dimitri.sabadie@gmail.com>
2015-11-01 14:49:27 +0100

    Migrated Renderbuffer to gl32.

--- src/Graphics/Luminance/Core/Renderbuffer.hs
+++ src/Graphics/Luminance/Core/Renderbuffer.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE CPP #-}
+
 -----------------------------------------------------------------------------
 -- |
 -- Copyright   : (C) 2015 Dimitri Sabadie
@@ -23,11 +25,22 @@ import Numeric.Natural ( Natural )
 newtype Renderbuffer = Renderbuffer { renderbufferID :: GLuint } deriving (Eq,Show)
 
 createRenderbuffer :: (MonadIO m,MonadResource m,Pixel f) => Natural -> Natural -> Proxy f -> m Renderbuffer
+#if GL45_BACKEND
 createRenderbuffer w h depthProxy = do
   rid <- liftIO . alloca $ \p -> do
     glCreateRenderbuffers 1 p
     rid <- peek p
     glNamedRenderbufferStorage rid (pixelIFormat depthProxy) (fromIntegral w) (fromIntegral h)
     pure rid
-  _ <- register . with rid $ glDeleteRenderbuffers 1
-  pure $ Renderbuffer rid
+  _ <- register $ with rid (glDeleteRenderbuffers 1)
+  pure (Renderbuffer rid)
+#elif GL32_BACKEND
+createRenderbuffer w h depthProxy = do
+  rid <- liftIO . alloca $ \p -> do
+    glGenRenderbuffers 1 p
+    rid <- peek p
+    glRenderbufferStorage GL_RENDERBUFFER (pixelIFormat depthProxy) (fromIntegral w) (fromIntegral h)
+    pure rid
+  _ <- register $ with rid (glDeleteRenderbuffers 1)
+  pure (Renderbuffer rid)
+#endif
