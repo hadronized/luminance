d5afa38b5c3826a98a33ea1479da57d1d3b2fb8a
Dimitri Sabadie <dimitri.sabadie@gmail.com>
2016-02-18 17:33:18 +0100

    Forgot that.

---
+++ src/Control/Some.hs
@@ -0,0 +1,20 @@
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE PolyKinds #-}
+
+-----------------------------------------------------------------------------
+-- |
+-- Copyright   : (C) 2015 Dimitri Sabadie
+-- License     : BSD3
+--
+-- Maintainer  : Dimitri Sabadie <dimitri.sabadie@gmail.com>
+-- Stability   : experimental
+-- Portability : portable
+----------------------------------------------------------------------------
+
+module Control.Some (
+    -- * Free existential quantification
+    Some(..)
+  ) where
+
+data Some :: (k -> *) -> * where
+  Some :: f a -> Some f
